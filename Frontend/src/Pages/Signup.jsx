2904e55cd41fb49e3743bb0c2104114046dec416
Ashu <140695297+aashoo-code@users.noreply.github.com>
2025-11-08 14:02:01 +0530

    Remove unused Signup.css import
    Removed import statement for Signup.css.

--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Signup.css";
 import { Link } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { handleError } from "../utils";
